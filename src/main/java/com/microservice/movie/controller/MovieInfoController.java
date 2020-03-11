ceb2cffee65458467b1fa5cc2ff5ed33f8cc0f9c
deepak kumar <debug7000@gmail.com>
2020-03-11 16:32:37 +0530

    moviedb

--- src/main/java/com/microservice/movie/controller/MovieInfoController.java
+++ src/main/java/com/microservice/movie/controller/MovieInfoController.java
@@ -1,21 +1,40 @@
 package com.microservice.movie.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.microservice.movie.model.Movie;
+import com.microservice.movie.model.MovieSummary;
 
 @RestController
 @RequestMapping("/movies")
 public class MovieInfoController {
 
-	@RequestMapping("/{movieId}")
+	
+	//hard coded commented
+	/*@RequestMapping("/{movieId}")
 	public Movie getMovie(@PathVariable("movieId") String movieId) {
 		
 		return new Movie(movieId,"radhe","radhe");
 		
-	}
+	}*/
+	
+	@Value("${api.key}")
+    private String apiKey;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping("/{movieId}")
+    public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
+        MovieSummary movieSummary = restTemplate.getForObject("https://api.themoviedb.org/3/movie/" + movieId + "?api_key=" +  apiKey, MovieSummary.class);
+        return new Movie(movieId, movieSummary.getTitle(), movieSummary.getOverview());
+
+    }
 	
 	
 
